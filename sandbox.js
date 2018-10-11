29e68d761d1d5c8c7322236651f87c160a9dd9cd
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2018-10-11 12:15:12 -0500

    enhancing school and student creation

--- sandbox.js
+++ sandbox.js
@@ -4,7 +4,7 @@ const connect = ()=> {
     return mongoose.connect('mongodb://localhost:27017/mydb')
 }
 
-const student = new mongoose.Schema(
+const studentSchema = new mongoose.Schema(
     {
         firstName: {
             type: String, // can use built-in JS primitives
@@ -19,22 +19,37 @@ const student = new mongoose.Schema(
                 type: Number
             }
         },
-        faveFoods: [{type: String}]
+        faveFoods: [{type: String}],
+        school: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'school'
+        }
     }, 
     {timestamps: true} // via mongoose, maybe mongo.
 )  
 
-const Student = mongoose.model('student', student)
+const schoolSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+const Student = mongoose.model('student', studentSchema)
+const School = mongoose.model('school', schoolSchema)
 
 
 connect()
     .then(async connection => {
-        const student = await Student.create({firstName:"Tim"}) 
-        console.log(student) // this is a mongoose document object, not just a json-y object. 
+        const school = await School.findOneAndUpdate({name: 'mlk elementary'}, {name: 'mlk elementary'}, {upsert:true, new:true}).exec() // exec is nice with exec'ing Promises. ... if something's tight to spec of Promises.
+        const student = await Student.create({firstName:"Tim", school:school._id})
 
-        const found = await Student.find({firstName:"Tim"})
-        const student1 = await Student.findById('asdfasdfasdf')
-        const student2 = await Student.findById('asdfasdfasdf')
+        const match = await Student.findById(student.id) // mongoose adds .id (i'm guessing we could have used)
+            .populate('school') // fills out refs
+            .exec()
+        console.log(match);
     })
     .catch( e => console.error(e))
 
+// findOneAndCreate can have upsert=true.
